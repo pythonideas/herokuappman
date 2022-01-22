16bf42ada247ff549c360621211087f8628d1d7f
pythonideas <rustbotchessapp@gmail.com>
2022-01-22 12:43:37 +0000

    Get account

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,109 @@ import fetch from "node-fetch";
 
 export { fetch };
 
-export class HerokuAccount {}
+const API_BASE_URL = "https://api.heroku.com";
+const DEFAULT_ACCEPT = "application/vnd.heroku+json; version=3";
+
+const MAX_APPS = 5;
+
+let VERBOSE = true;
+
+function log(...args) {
+  if (VERBOSE) console.log(...args);
+}
+
+//////////////////////////////////////////////////////////////////////
+// API primitives
+
+export function api(endpoint, method, payload, token, accept?) {
+  const url = `${API_BASE_URL}/${endpoint}`;
+
+  const headers = {
+    Authorization: `Bearer ${token}`,
+    Accept: accept || DEFAULT_ACCEPT,
+    "Content-Type": "application/json",
+  };
+
+  const body = payload ? JSON.stringify(payload) : undefined;
+
+  if (require.main === module) {
+    log({ endpoint, method, url, headers, payload, token, body });
+  }
+
+  return new Promise((resolve, reject) => {
+    fetch(url, {
+      method,
+      headers,
+      body,
+    }).then(
+      (resp) =>
+        resp.json().then(
+          (json) => resolve(json),
+          (err) => {
+            console.error(err);
+            reject(err);
+          }
+        ),
+      (err) => {
+        console.error(err);
+        reject(err);
+      }
+    );
+  });
+}
+
+function get(endpoint, payload, token, accept?) {
+  return api(endpoint, "GET", payload, token, accept);
+}
+
+function post(endpoint, payload, token, accept?) {
+  return api(endpoint, "POST", payload, token, accept);
+}
+
+function del(endpoint, payload, token, accept?) {
+  return api(endpoint, "DELETE", payload, token, accept);
+}
+
+function patch(endpoint, payload, token, accept?) {
+  return api(endpoint, "PATCH", payload, token, accept);
+}
+
+//////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////
+
+export class HerokuAccount {
+  name: string = "";
+  envTokenFullName: string = "";
+  token: string = "";
+  id: string = "";
+  constructor(name: string) {
+    this.name = name;
+    this.envTokenFullName = "HEROKU_TOKEN_" + this.name;
+    this.token = process.env[this.envTokenFullName];
+  }
+  getAccount() {
+    return new Promise((resolve) => {
+      get(`account`, undefined, this.token).then((json: any) => {
+        this.id = json.id;
+        resolve(json);
+      });
+    });
+  }
+  init() {
+    return new Promise(async (resolve) => {
+      const account = await this.getAccount();
+      resolve({ account });
+    });
+  }
+}
 
 async function test() {
-  const resp = await fetch("https://lichess.org");
-  const text = await resp.text();
-  console.log(text);
+  const acc = new HerokuAccount("BROWSERCAPTURES");
+
+  const initResult = await acc.init();
+
+  console.log(initResult, acc);
 }
 
 test();
